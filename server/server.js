5e890e1f135e76945a03eda2ada642d8e7e7d354
chrisgth <ksemkus00@gmail.com>
2022-07-11 20:09:09 +0300

    passport rendered obsolete in this case so removed

--- server/server.js
+++ server/server.js
@@ -1,13 +1,6 @@
 const express = require("express");
-const path = require("path");
-const session = require("express-session");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
-const bcryptjs = require('bcryptjs');
-const User = require('./models/user.js')
 const userRoutes = require('./routes/users.js')
-const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const server = express()
@@ -20,46 +13,6 @@ mongoose
 	})
 	.catch(err => console.log(err))
 
-server.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
-server.use(passport.initialize());
-server.use(passport.session());
 server.use(express.urlencoded({ extended: false }));
-
-passport.use(
-  new LocalStrategy((username, password, done) => {
-    User.findOne({ username: username }, (err, user) => {
-      if (err) { 
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      }
-			bcryptjs.compare(password, user.password, (err, res) => {
-				if (res) {
-					// passwords match! log user in
-					return done(null, user)
-				} else {
-					// passwords do not match!
-					return done(null, false, { message: "Incorrect password" })
-				}
-			})
-    });
-  })
-);
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-
-server.use(function(req, res, next) {
-  res.locals.currentUser = req.user;
-  next();
-});
 
 server.use('/api/user', userRoutes)
