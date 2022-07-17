7fc4847a47fe60753ab60f7b359c09af5d90e5e6
chrisgth <ksemkus00@gmail.com>
2022-07-18 01:52:59 +0300

    added api config and get request

--- app/src/pages/Dashboard.js
+++ app/src/pages/Dashboard.js
@@ -1,19 +1,37 @@
+import axios from "axios";
 import { useState } from "react";
 import Search from "../images/loupe.png";
-
 const Dashboard = ({ user }) => {
+  const getFood = async (food) => {
+    const response = await axios.get(
+      "https://api.edamam.com/api/food-database/v2/parser",
+      {
+        params: {
+          app_id: process.env.REACT_APP_APP_ID,
+          app_key: process.env.REACT_APP_APP_KEY,
+          ingr: food,
+        },
+      }
+    );
+
+    return response;
+  };
+
   const [searchbar, setSearchBar] = useState(false);
+  const [searchQuery, setSearchQuery] = useState();
   const trackuser = () => {
     console.log(user);
   };
-  const changeHandler = () => {
+  const changeHandler = (e) => {
     const input = document.getElementById("searchbox");
     if (input.value === "") {
       setSearchBar(false);
     } else {
+      setSearchQuery(e.target.value);
       setSearchBar(true);
     }
   };
+
   return (
     <div className="dashboard">
       <div className="sidebar"></div>
