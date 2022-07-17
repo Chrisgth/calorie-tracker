84b7d0242726e20ee8f4df686d4465bf637f2717
chrisgth <ksemkus00@gmail.com>
2022-07-18 00:43:38 +0300

    created dropdown for search bar

--- app/src/pages/Dashboard.js
+++ app/src/pages/Dashboard.js
@@ -1,14 +1,45 @@
+import { useState } from "react";
 import Search from "../images/loupe.png";
+
 const Dashboard = ({ user }) => {
+  const [searchbar, setSearchBar] = useState(false);
   const trackuser = () => {
     console.log(user);
+  };
+  const changeHandler = () => {
+    const input = document.getElementById("searchbox");
+    if (input.value === "") {
+      setSearchBar(false);
+    } else {
+      setSearchBar(true);
+    }
   };
   return (
     <div className="dashboard">
       <div className="sidebar"></div>
       <div className="searchbar">
-        <img src={Search} alt="" />
-        <input type="text" placeholder="Search for food here..." />
+        <div className="search">
+          <img src={Search} alt="" />
+          <input
+            id="searchbox"
+            type="text"
+            placeholder="Search for food here..."
+            onChange={changeHandler}
+          />
+        </div>
+        {searchbar && (
+          <div className="dropdown">
+            <ul>
+              <li>A</li>
+              <li>A</li>
+              <li>A</li>
+              <li>A</li>
+              <li>A</li>
+              <li>A</li>
+              <li>A</li>
+            </ul>
+          </div>
+        )}
       </div>
       <div className="display"></div>
     </div>
